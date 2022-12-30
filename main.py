0870fa93ce08369c9d8242806a159ca6a140fe34
Sebyeol23 <lixxil2323@gmail.com>
2022-12-30 14:37:04 +0900

    setTimeout

--- main.py
+++ main.py
@@ -18,4 +18,26 @@ def get_root():
             n = random.randrange(0, 10)
 
         ans.append(n)
+    print(ans)
     return FileResponse("index.html")
+
+@app.get("/check")
+def get_check(number: str):
+    ball = 0
+    strike = 0
+    
+    for i in range(len(number)):
+        if int(number[i]) in ans:
+            if int(number[i]) == ans[i]:
+                strike += 1
+            else:
+                ball += 1
+
+    table = ["OUT", "HIT", "HIT", "HIT", "HIT"]
+
+    if strike == 4:
+        data = {"result": "HOMERUN", "ball": ball, "strike": strike}
+    else:
+        data = {"result": table[ball+strike], "ball": ball, "strike": strike}
+
+    return data
